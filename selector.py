50f7ce9910fac0ab807195f56d504f5c4b5bafa9
Naidraug0 <kyungchan7891@gmail.com>
2023-05-01 14:44:12 +0900

    feat:반복

--- selector.py
+++ selector.py
@@ -2,7 +2,10 @@ import random
 
 
 def get_lucky_nums():
-    return random.sample(range(1,45+1), k=6
+    return random.sample(range(1,45+1), k=6)
 
-if __name__=='__main__':
-    print(get_lucky_nums())
+
+if __name__ == '__main__':
+    times = int(input('Enter num(1-100): '))
+    for _ in range(times):
+        print(get_lucky_nums())
